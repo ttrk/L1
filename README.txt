b4a4665bc9b40478a82b1ec78c58251a2372a8d9
R. Alex Barbieri <luck@mit.edu>
2015-03-10 14:33:00 -0400

    add Turn-On curve plotting code

--- README.txt
+++ README.txt
@@ -1,8 +1,8 @@
 Set of macros to run L1 Jet algorithm on top of previous L1 results.
 
 Instructions:
-The macro L1JetEmulator.C contains a copy of the L1 jet emulator. It expects as input a file made by the L1UpgradeAnalyzer code, but only uses the region information. To test a change to the L1 jet algorithm, modify this macro and use runL1JetEmulator.sh to run it over both the Hydjet 2.76 and 5.02 samples.
+The macro L1JetEmulator.C contains a copy of the L1 jet emulator. It expects as input a file made by the L1UpgradeAnalyzer code, but only uses the region information. To test a change to the L1 jet algorithm, modify this macro and use runL1JetEmulator.sh to run it.
 
-Once the output is created, you can use the makeRateCurve.C macro to draw the L1 Jet rate as a function of physical pT in GeV.
+Once the output is created, you can use the makeRateCurve.C macro to draw the L1 Jet rate as a function of physical pT in GeV. It can be run interactively in a root session to draw the rate curve, or in batch mode which will dump the rates to text.
 
-The macro drawTurnOnCurves.C will also draw a set of Turn-on curves and requires event matching between a HiForest file and the output of the L1 Jet emulator. You can change the L1 thresholds inside the macro.
+The macro makeTurnOnCurves.C will also draw a set of Turn-on curves and requires event matching between a HiForest file and the output of the L1 Jet emulator. You can change the L1 thresholds inside the macro.
--- runL1JetEmulator.sh
+++ runL1JetEmulator.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
+
+# This is a master file to run the emulator macro and then print out rates on multiple datasets at once
+# You can find the up-to-date list of samples here: https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideL1TValidationSamples
+
 # compile the macro with g++
 g++ L1JetEmulator.C $(root-config --cflags --libs) -Werror -Wall -Wextra -O2 -o L1JetEmulator.exe || exit 1
 
-./L1JetEmulator.exe "/export/d00/scratch/luck/Hydjet1p8_2760GeV_L1UpgradeAnalyzer_GT_run1_mc_HIon_L1UpgradeAnalyzer.root" "Hydjet276_JetResults.root" || exit 1
-./L1JetEmulator.exe "/export/d00/scratch/luck/HydjetMB_740pre8_MCHI2_74_V3_53XBS_L1UpgradeAnalyzer_GT_MCHI2_74_V3.root" "Hydjet502_JetResults.root" || exit 1
+./L1JetEmulator.exe "/export/d00/scratch/luck/Hydjet1p8_2760GeV_L1UpgradeAnalyzer_GT_run1_mc_HIon_L1UpgradeAnalyzer.root" "~/scratch/Hydjet276_JetResults.root" || exit 1
+./L1JetEmulator.exe "/export/d00/scratch/luck/HydjetMB_740pre8_MCHI2_74_V3_53XBS_L1UpgradeAnalyzer_GT_MCHI2_74_V3.root" "~/scratch/Hydjet502_JetResults.root" || exit 1
 
 g++ makeRateCurve.C $(root-config --cflags --libs) -Werror -Wall -Wextra -O2 -o makeRateCurve.exe || exit 1
 
-./makeRateCurve.exe "Hydjet276_JetResults.root" || exit 1
-./makeRateCurve.exe "Hydjet502_JetResults.root" || exit 1
+./makeRateCurve.exe "~/scratch/Hydjet276_JetResults.root" || exit 1
+./makeRateCurve.exe "~/scratch/Hydjet502_JetResults.root" || exit 1
+
+g++ makeTurnOn.C $(root-config --cflags --libs) -Werror -Wall -Wextra -O2 -o makeTurnOn.exe || exit 1
+
+./makeTurnOn.exe "~/scratch/Hydjet276_JetResults_zeroWalls.root" "/mnt/hadoop/cms/store/user/ginnocen/Hydjet1p8_TuneDrum_Quenched_MinBias_2760GeV/HiMinBias_Forest_26June2014/d9ab4aca1923b3220eacf8ee0d550950/*.root" "hist_hydjet276.root" 1 || exit 1
