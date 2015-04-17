4f9d4ae0ea19fa36b64915aee522bac175937669
KayaUser <kaya@yahoo.com>
2015-04-17 14:49:53 -0400

    I do not need to display the run information from those scripts any more.

--- runL1JetEmulator_my_zeroOut.sh
+++ runL1JetEmulator_my_zeroOut.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-############################################################
-## print the command
-echo "command called =" $0 $1 $2
-## start time of the script
-date1=$(date +"%s")         # %s     seconds since 1970-01-01 00:00:00 UTC
-#date1Human=$(date -d @$date1)
-date1Human=$(date -d @$date1 '+%d/%m/%Y %H:%M:%S')
-echo "started on     =" $date1Human
-echo "############################################################"
-############################################################
 
 # The input files are listed here : https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideL1TValidationSamples
 
@@ -71,21 +61,3 @@ do
     done
 done
 
-############################################################
-echo "############################################################"
-echo "##### log of the SCRIPT =" $0
-
-## finish time and run time of the script
-date2=$(date +"%s")         # %s     seconds since 1970-01-01 00:00:00 UTC
-#date2Human=$(date -d @$date2)
-date2Human=$(date -d @$date2 '+%d/%m/%Y %H:%M:%S')
-diff=$(($date2-$date1))
-diffHuman=$(date -d @$diff)
-#echo $date1
-echo "started  on    =" $date1Human
-echo "finished on    =" $date2Human
-echo "$(($diff / 60)) minutes and $(($diff % 60)) seconds elapsed."
-echo $diffHuman
-
-echo "##### END #####"
-############################################################
