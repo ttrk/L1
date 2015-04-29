3a518d8fa1f63cc8c8cc682b6cf2613bc0644518
KayaUser <kaya@yahoo.com>
2015-04-29 16:16:36 -0400

    checked out my "scp_*.h" scripts

---
+++ scp_to_cgate_L1.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#http://stackoverflow.com/questions/3598664/creating-a-shell-script-to-run-java-program?rq=1
+
+#sourceDir="L1EmulatorMacros"
+sourceFiles="
+*.C
+*.h
+*.sh
+*.txt
+";         # do not copy all sort of files
+remoteMachine="tatar@cgate.mit.edu";
+destinationInRemote="~/CMSSW_7_4_0_pre6/src/L1EmulatorMacros";
+destination=$remoteMachine":"$destinationInRemote;
+
+scp $sourceFiles $destination
